564cf55b8f1e5d1b43d8fd6329a4363fd0547c57
don12 <gob669668@gmail.com>
2025-05-29 15:28:39 +0800

    123

--- MVC(DB)/MVC(DB)/obj/Debug/net6.0/MVC(DB).AssemblyInfo.cs
+++ MVC(DB)/MVC(DB)/obj/Debug/net6.0/MVC(DB).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC(DB)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7ffac1c9e6c435d1ad566e9e2933704af29ded8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d14b1d2a6bcb32ae84e5043861916ba78aa5e70")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC(DB)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC(DB)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
